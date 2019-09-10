84bd589669c0920a3f324eba33b311d5dff044b6
AndyPasic <ajpasic@gmail.com>
2019-09-09 17:27:07 -0700

    Adding configuration files.

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+ #!/bin/bash
+ git config --global user.email "$GIT_EMAIL"
+ git config --global user.name "$GIT_ACCOUNT"
+ cd public
+ git add --all
+ git commit -m "Publishing to gh-pages"
+ cd ..
+ git push https://$GIT_ACCOUNT:$GIT_KEY@github.com/$GIT_PROJECT gh-pages
